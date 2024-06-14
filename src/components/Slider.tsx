1bfafa0215f0280c64ed24539067f2e7e3ee95a8
Jeremi Wielewski <jerwier420@gmail.com>
2024-06-14 09:15:08 +0200

    change slider styles

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,27 +10,25 @@ import {
 
 export function Slider() {
   return (
-    <div className="bg-gradient-to-r from-purple-600 to-pink-600 p-12 rounded-lg flex justify-center">
-      <AriaSlider defaultValue={30} className="w-[250px]">
-        <div className="flex text-white">
-          <Label className="flex-1">Opacity</Label>
-          <SliderOutput />
-        </div>
-        <SliderTrack className="relative w-full h-7">
-          {({ state }) => (
-            <>
-              {/* track */}
-              <div className="absolute h-2 top-[50%] translate-y-[-50%] w-full rounded-full bg-white/40" />
-              {/* fill */}
-              <div
-                className="absolute h-2 top-[50%] translate-y-[-50%] rounded-full bg-white"
-                style={{ width: state.getThumbPercent(0) * 100 + "%" }}
-              />
-              <SliderThumb className="h-7 w-7 top-[50%] rounded-full border border-solid border-purple-800/75 bg-white transition dragging:bg-purple-100 outline-none focus-visible:ring-2 ring-black" />
-            </>
-          )}
-        </SliderTrack>
-      </AriaSlider>
-    </div>
+    <AriaSlider defaultValue={30}>
+      <div className="hidden">
+        <Label className="flex-1">Opacity</Label>
+        <SliderOutput />
+      </div>
+      <SliderTrack className="relative w-full h-7">
+        {({ state }) => (
+          <>
+            {/* track */}
+            <div className="absolute h-1 top-[50%] translate-y-[-50%] w-full rounded-full bg-white/30" />
+            {/* fill */}
+            <div
+              className="absolute h-1 top-[50%] translate-y-[-50%] rounded-full bg-white"
+              style={{ width: state.getThumbPercent(0) * 100 + "%" }}
+            />
+            <SliderThumb className="h-2 w-2 top-[50%] rounded-full cursor-pointer border border-solid border-purple-800/75 bg-white transition dragging:bg-purple-100 outline-none focus-visible:ring-2 ring-black" />
+          </>
+        )}
+      </SliderTrack>
+    </AriaSlider>
   );
 }
