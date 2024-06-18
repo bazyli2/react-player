5f88fde1494a2a594ea88108d90900f2ecbf3a07
Jeremi Wielewski <jeremi.wielewski@profil-software.com>
2024-06-18 12:24:07 +0200

    Added playing and pausing video

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Slider } from "./Slider";
 import Image from "next/image";
 import image from "./chilling-sunday.jpg";
@@ -7,55 +8,80 @@ import { FastForward } from "./icons/FastForward";
 import { Play } from "./icons/Play";
 import { VolumeDown } from "./icons/VolumeDown";
 import { VolumeUp } from "./icons/VolumeUp";
+import { useEffect, useRef, useState } from "react";
+import { ToggleButton } from "react-aria-components";
+import { Pause } from "./icons/Pause";
 
 export function Player() {
+  const url =
+    "https://file-examples.com/storage/fed5266c9966708dcaeaea6/2017/11/file_example_MP3_5MG.mp3";
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  useEffect(() => {
+    if (audioRef.current === null) return;
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+  const toggleIsPlaying = (isPlaying: boolean) => {
+    setIsPlaying(isPlaying);
+  };
   return (
-    <div className="p-4 rounded-2xl bg-black/60 max-w-80">
-      <div className="flex items-center gap-3">
-        <Image
-          alt="album cover"
-          className="rounded-lg"
-          {...image}
-          height={100}
-          width={100}
-        />
-        <div className="overflow-hidden">
-          <h2 className="text-xs tracking-wide leading-7 font-medium block">
-            Jun Pulse
-          </h2>
-          <h3 className="text-base font-bold block leading-6 tracking-normal overflow-ellipsis overflow-hidden whitespace-nowrap">
-            คนเก่าเขาทำไว้ดี (Can't win)
-          </h3>
-          <h4 className="text-base tracking-tight overflow-ellipsis overflow-hidden whitespace-nowrap">
-            Chilling Sunday — คนเก่าเขาทำไว้ดี
-          </h4>
+    <>
+      <div className="p-4 rounded-2xl bg-black/60 max-w-80">
+        <div className="flex items-center gap-3">
+          <Image
+            alt="album cover"
+            className="rounded-lg"
+            {...image}
+            height={100}
+            width={100}
+          />
+          <div className="overflow-hidden">
+            <h2 className="text-xs tracking-wide leading-7 font-medium block">
+              Jun Pulse
+            </h2>
+            <h3 className="text-base font-bold block leading-6 tracking-normal overflow-ellipsis overflow-hidden whitespace-nowrap">
+              คนเก่าเขาทำไว้ดี (Can&apos;t win)
+            </h3>
+            <h4 className="text-base tracking-tight overflow-ellipsis overflow-hidden whitespace-nowrap">
+              Chilling Sunday — คนเก่าเขาทำไว้ดี
+            </h4>
+          </div>
+        </div>
+        <Slider />
+        <div className="flex justify-between -mt-2">
+          <span className="text-xs leading-5 tracking-wide text-slate-500">
+            0:00
+          </span>
+          <span className="text-xs leading-5 tracking-wide text-slate-500">
+            -3:20
+          </span>
+        </div>
+        <div className="flex justify-center items-center">
+          <IconButton>
+            <FastRewind className="text-[35px]" />
+          </IconButton>
+          <ToggleButton
+            isSelected={isPlaying}
+            onChange={toggleIsPlaying}
+            className="text-[42px]"
+          >
+            {({ isSelected }) => (isSelected ? <Pause /> : <Play />)}
+          </ToggleButton>
+          <IconButton>
+            <FastForward className="text-[35px]" />
+          </IconButton>
+        </div>
+        <div className="flex gap-4 items-center">
+          <VolumeDown className="text-2xl" />
+          <Slider className="flex-1" />
+          <VolumeUp className="text-2xl" />
         </div>
       </div>
-      <Slider />
-      <div className="flex justify-between -mt-2">
-        <span className="text-xs leading-5 tracking-wide text-slate-500">
-          0:00
-        </span>
-        <span className="text-xs leading-5 tracking-wide text-slate-500">
-          -3:20
-        </span>
-      </div>
-      <div className="flex justify-center items-center">
-        <IconButton>
-          <FastRewind className="text-[35px]" />
-        </IconButton>
-        <IconButton>
-          <Play className="text-[42px]" />
-        </IconButton>
-        <IconButton>
-          <FastForward className="text-[35px]" />
-        </IconButton>
-      </div>
-      <div className="flex gap-4 items-center">
-        <VolumeDown className="text-2xl" />
-        <Slider className="flex-1" />
-        <VolumeUp className="text-2xl" />
-      </div>
-    </div>
+      <audio src={url} ref={audioRef} />
+    </>
   );
 }
